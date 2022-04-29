534331bedef06a96551c5d2b9f09808728d9036d
Braulio <b.oyarzun.barros@gmail.com>
2022-04-29 13:09:48 -0400

    recursive relation for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :bookmarks
+
+  belongs_to :parent, class_name: "Category", foreign_key: "parent_category_id", optional: true
+  has_many :children, class_name: "Category", foreign_key: "parent_category_id"
 end
