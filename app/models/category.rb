b1981944e0fe913291fadbcb8feb4b28ef2c55f2
Braulio <b.oyarzun.barros@gmail.com>
2022-04-30 12:59:04 -0400

    bootstrap with js dropdowns for bookmarks folders

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,6 @@ class Category < ApplicationRecord
   # TODO validates - not allowed self referencing
   belongs_to :parent, class_name: "Category", optional: true
   has_many :children, class_name: "Category", foreign_key: "parent_category_id", dependent: :destroy
+
+  scope :root_parents, -> { where(parent_category_id: nil) }
 end
